c4f7e33ce2cc75943af31de284ace1ec1e49d09f
mokshshah18 <acams1803@gmail.com>
2022-12-02 03:05:02 -0500

    Update cookie.py
    todo :
    https://stackoverflow.com/questions/45495344/how-to-increase-a-variable-every-second-in-pygame

--- cookie.py
+++ cookie.py
@@ -1,28 +1,66 @@
-# Importing pygame module
+# Importing
 import pygame
+import time
  
-# initiate pygame and give permission
-# to use pygame's functionality.
+# initiatizing
 pygame.init()
- 
-# create the display surface object
-# of specific dimension.
-screen = pygame.display.set_mode((800, 800))
- 
-# Fill the scree with white color
+#screen variables
+""" screen = pygame.display.set_mode() # Default to screen resolution.
+area = screen.get_rect()
+print(area) """
+screen = pygame.display.set_mode()
 screen.fill((255, 255, 255))
+
+#design variables
+pygame.display.set_caption('cookie clicker')
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+#game variables
+score = 10
 running = True
+cramperBrought = {
+    "granny" : 0,
+} #amount of bots bought key is the diffrent bots availbile and values are the amount bought
+cookieCounterG=0                                                      
 while running:
+
+    #game shapes and colliders
+    granny = pygame.Rect(200,900,700,900)
+
+
+    scorep = "Cookies = " + str(score) 
+    ScoreText = font.render(scorep,True,(10,10,10))
+    Instructions = ["Get to 10 cookies to start automating","welcome to automation land"]
+    if(score<10):                                                         
+        InstructionText = font.render(Instructions[0],True,(0,0,0))
+    else:
+        InstructionText = font.render(Instructions[1],True,(0,0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.K_ESCAPE:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            bool_mokey_1 = True
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:#right mouse button events
+            #msx, msy get mouse pointer variables
             mouse_x, mouse_y = pygame.mouse.get_pos()
             msx,msy = mouse_x,mouse_y
-
-    pygame.draw.circle(screen, (0,0,0), (400, 400), 100, 10)
-    # Draws the surface object to the screen.
+            if(msx<500 and msx>300 and msy>300 and msy<500):#circle hitbox?
+                score += 1
+            if(score>10 and granny.collidepoint(msx,msy)):                                            
+                cramperBrought["granny"] += cramperBrought.get("granny") + 1
+                score -= 5
+                start_time = time.time()
+                cookieCounterG = time.time()-start_time * cramperBrought["granny"]
+    if (cramperBrought.get("granny")!=0):
+        score = score + cookieCounterG
+    #drawing
+    pygame.draw.circle(screen, (0,0,100), (400, 400), 100, 100)
+    screen.blit(ScoreText,(10,10))#displays text
+    screen.blit(InstructionText,(70,600))
+    if(score>10):
+        pygame.draw.rect(screen,(0,0,0),granny)
+    #update
     pygame.display.update()
+    #clean screen
+    screen.fill((255, 255, 255))
